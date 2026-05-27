e8e73868995a9e899d2fede45824d245d01bb014
lelebettiol31-ops <lelebettiol31@gmail.com>
2026-05-27 19:33:57 -0300

    Create teste_calculadora.c

---
+++ teste_calculadora.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdio.h>
+#include "function_divisao.h"
+
+int main() {
+
+    assert(dividir(14,2) == 7);
+    assert(dividir(99,3) == 33);
+    assert(dividir(144,12) == 12);
+
+    printf("Todos os testes passaram!\n");
+
+    return 0;
+}
